d3563d0b3c8bc7e542b8704767c8b958a1db1388
Ness Bautista <ness@Nesss-MacBook-Pro.local>
2020-07-22 16:55:46 -0500

    celcius_to_fahrenheit python file

--- HandsOnML/validationInstalls.py
+++ HandsOnML/validationInstalls.py
@@ -2,7 +2,11 @@ import matplotlib
 import numpy as np
 import tensorflow as tf
 import pandas as pd
+import sklearn
+
 print(tf.__version__)
 print(np.version)
 print(matplotlib.__version__)
 print(pd.show_versions())
+print("sklearn version:")
+print(sklearn.__version__)
