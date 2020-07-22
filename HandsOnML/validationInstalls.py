15719f0ef65acadd98ad6227f93effdca6a035d9
Ness Bautista <ness@Nesss-MacBook-Pro.local>
2020-07-22 16:30:58 -0500

    Added exercise files from HandsOnML

---
+++ HandsOnML/validationInstalls.py
@@ -0,0 +1,8 @@
+import matplotlib
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+print(tf.__version__)
+print(np.version)
+print(matplotlib.__version__)
+print(pd.show_versions())
